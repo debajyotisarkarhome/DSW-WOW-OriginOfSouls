f53821df5a32929306bf200ed1459c2fab6a811d
Debajyoti sarkar <debajyotisarkar.jio1@gmail.com>
2020-12-11 16:43:31 +0530

    Create web_blocker.py (onky windows)
    Created the main blocking and unblocking functions for windows only

---
+++ web_blocker.py
@@ -0,0 +1,21 @@
+import platform
+def block(sites):
+    osp=platform.platform()
+    osp=osp.lower()
+    if osp[:3]=="win":
+        host_handler=open("C:\\Windows\\System32\\drivers\\etc\\hosts","a+")
+        host_data=host_handler.read()
+        #print(host_data)
+        for i in sites:
+            if str(host_data.find(i))=="-1":
+                host_handler.write('''127.0.0.1 '''+i+"\n")
+                host_handler.write('''127.0.0.1 www.'''+i+"\n")
+        host_handler.close()
+def unblock():
+    osp=platform.platform()
+    osp=osp.lower()
+    if osp[:3]=="win":
+        host_handler=open("C:\\Windows\\System32\\drivers\\etc\\hosts","w+")
+        host_data=host_handler.read()
+        host_handler.write("")
+        host_handler.close()
